4cecbb25b9e3ff4254a2f4b50a5682049c84eac2
Anikcse19 <anikdebcse@gmail.com>
2024-08-18 02:21:39 +0600

    hidden nav on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        <FloatingNav className="hidden md:block" navItems={navItems} />
         <Hero />
         <Grid />
         <AboutMe />
